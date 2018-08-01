3a1dbfef42f894a7e160a204a18563f729e216ad
AnonymousICT <kuohoward@gmail.com>
2018-08-01 13:31:36 -0700

    made results show damage_relations from each type

--- main.js
+++ main.js
@@ -7,42 +7,68 @@ $.getJSON(results, callback)
 }
 
 function getTypeData(array, callback) {
+	console.log('yes I crossDomain');
 // let typeResults = `${POKEMON_URL}/type/${value.type}`
-$.each(array, function( index , value ) {
-$.ajax({
-    url: 'https://pokemonurl.com',
-    type: 'GET',
-    crossDomain: true,
-    dataType: 'jsonp',
-    success: success
-});
- 
-});
+	$.each(array, function( index , value ) {
+		console.log(value);
+		console.log(`${POKEMON_URL}/type/${value}`)
+		$.ajax({
+		    url: `${POKEMON_URL}/type/${value}`,
+		    type: 'GET',
+		    crossDomain: true,
+		    success: callback
+		}); 
+	});
 }
 
 function renderTypeResults(data){
+// console.log('test');
+	let damageArray = [];
+	$.each(data.damage_relations , function(index , value) {
+		$.each(value, function(index , damageObject) {
+			// console.log(damageObject)
+			damageArray.push("<li>"+index+"</li>")
+			damageArray.push("<li>"+damageObject.name+"</li>")
+		} )
+	})
+	let halfDamageArray=[];
+	console.log("APPLES");
+	console.log(halfDamageArray);
+	$.each(data.damage_relations.half_damage_from, function(index, halfDamage){
+		// halfDamageArray.push("<h2>"+halfDamage.name+"</h2>");
+		console.log('BANANA');
+		console.log(halfDamageArray);
+	})
 return `
 <div class='resultsType'>
+	<p> this works</p>
+	${halfDamageArray.join()};
 	<ul>
-		<li>${data.damage_relations.half_damage_from.name}</li>
+		${damageArray.join()};
 	</ul>
 </div>
 `
 }
 
 function displayTypeData(data){
-	$('.results').html(renderTypeResults(data));
+	console.log(data);
+	console.log(data.damage_relations);
+	console.log ("THIS IS ALL WE WANTED WE DO, DAMMIT");
+	for (let i=0; i<data.damage_relations.double_damage_from.length; i++) {
+		console.log(i, data.damage_relations.double_damage_from[i].name);
+	}
+	$('.typeResults').html(renderTypeResults(data));
 }
 
 function renderResults(data) {
-console.log(data)
-let pokemonNumber = `${data.id}`
-let pokemonName = `${data.name}`
-let pokemonType = ''
+console.log(data);
+let pokemonNumber = `${data.id}`;
+let pokemonName = `${data.name}`;
+let pokemonType = '';
 $.each(data.types, function( index , value ) {
   pokemonType += `<li>${value.type.name}</li>`
   POKEMONTYPE_ARRAY.push(value.type.name);
-  console.log(POKEMONTYPE_ARRAY)
+  console.log(POKEMONTYPE_ARRAY);
 });
 
 return `<div class='resultsName'>${data.id}
@@ -55,6 +81,7 @@ return `<div class='resultsName'>${data.id}
 
 function displaySearchResults (data) {
 	$('.results').html(renderResults(data));
+	// console.log('SEE IT?!')
   	getTypeData(POKEMONTYPE_ARRAY, displayTypeData)
 }
 
@@ -63,17 +90,21 @@ $('.form').on('submit', function(event){
 	event.preventDefault();
 	const userInput = $
 	(event.currentTarget).find('.userInput').val().toLowerCase()
-	console.log(userInput);
+	// console.log(userInput);
 	getDataFromApi(userInput, displaySearchResults);
 })
 }
 
 function listenRandomPokemonButton () {
-
+$('randomButton').click(function(){
+	let pokemonNumber = Math.floor(Math.random() * Math.floor(802));
+	renderResults();
+})
 }
 
 function init() {
 	listenSubmitButton()
+	listenRandomPokemonButton()
 
 }
 
